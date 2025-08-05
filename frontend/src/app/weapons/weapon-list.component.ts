fd45ce31619639f4e7263913e6c5e26623d31254
daniloladeira <daniloladeira20@gmail.com>
2025-08-05 11:36:11 -0300

    feat: replace welcome table with a simple div and add weapon-related components

---
+++ frontend/src/app/weapons/weapon-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-weapon-list',
+  template: `
+      
+    `
+})
+
+export class NameComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
